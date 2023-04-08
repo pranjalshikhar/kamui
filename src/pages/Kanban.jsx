a34ceafa6b0d8c1d7a148d6b034b89ee90100f70
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-04-09 01:29:08 +0530

    Update Kanban.jsx

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -184,7 +184,7 @@ const Kanban = ({ userId }) => {
 
                     <div className="flex items-center text-blue-900 hover:bg-blue-600 hover:text-blue-50 bg-transparent transition duration-200 cursor-pointer hover:scale-110 rounded-sm px-2 py-1 mr-3 sm:flex text-2xl">
                       <a
-                        href="https://github.com/lucky-chap/Kamui"
+                        href="https://github.com/pranjalshikhar/kamui"
                         target="_blank"
                         rel="noreferrer"
                       >
